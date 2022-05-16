3893b2282d705e03c6ff1c636184f233b187791d
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-05-16 13:13:01 -0400

    S3.26 error notification

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
-//S3.25
-const observable$ = new Observable<string>(subscriber => {  
-  console.log('Observable executed');
-  subscriber.next('Alice');
-  subscriber.next('Ben');
+//S3.26
+const observable$ = new Observable<string>((subscriber) => {
+  console.log("Observable executed");
+  subscriber.next("Alice");
+  subscriber.next("Ben");
   setTimeout(() => {
-    subscriber.next('Charlie');
-    console.log('setting complete event');
-    subscriber.complete();
-}, 2000);
+    subscriber.next("Charlie");
+  }, 2000);
+  setTimeout(() => subscriber.error(new Error('Failure')), 4000);
 
-return () => {
-  console.log('Teardown');
-};
+  return () => {
+    console.log("Teardown");
+  };
 });
-console.log('Before subscribe');
+console.log("Before subscribe");
 observable$.subscribe({
-  next: (value) => console.log('Subscription executed', value),
-  complete: () => console.log('Subscription completed')
+  next: (value) => console.log("Subscription executed", value),
+  error: (err) => console.log(err.message),
+  complete: () => console.log("Subscription completed")
 });
-console.log('After subscribe');
+console.log("After subscribe");
