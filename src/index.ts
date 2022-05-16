2fb3d883f36981384164b221e19da805641d3040
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-05-16 15:02:46 -0400

    S4.33 hot observable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Observable } from "rxjs";
 import { ajax } from "rxjs/ajax";
 
-//S4.32
-const ajax$ = ajax<any>("https://random-data-api.com/api/name/random_name");
+//S4.33
 
-ajax$.subscribe((data: any) => console.log("Sub 1:", data.response.first_name));
-ajax$.subscribe((data: any) => console.log("Sub 2:", data.response.first_name));
-ajax$.subscribe((data: any) => console.log("Sub 3:", data.response.first_name));
+const helloButton = document.querySelector("button#hello");
+
+const helloClick$ = new Observable<MouseEvent>((subscriber) => {
+  helloButton.addEventListener("click", (event) => {
+    subscriber.next(event as MouseEvent);
+  });
+});
+
+helloClick$.subscribe((event: MouseEvent) => console.log('Sub 1:', event.type, event.x, event.y));
+
+setTimeout(() => {
+  console.log('Subscription 2 starts');
+  helloClick$.subscribe((event: MouseEvent) => console.log('Sub 2:', event.type, event.x, event.y));
+}, 5000);
