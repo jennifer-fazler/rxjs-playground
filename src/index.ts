245f55899cc916776bf2397ba4aa70546b2ac035
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-05-16 16:56:13 -0400

    S5.38 from

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import { Observable, of } from "rxjs";
+import { from } from "rxjs";
 
-//S5.37
-// of('Alice', 'Ben', 'Charlie').subscribe({
-ourOwnOf("Alice", "Ben", "Charlie").subscribe({
-  next: (value) => console.log(value),
-  complete: () => console.log("Completed"),
+// from(['Alice', 'Ben', 'Charlie']).subscribe({
+//   next: value => console.log(value),
+//   complete: () => console.log('Completed')
+// });
+
+const somePromise = new Promise((resolve, reject) => {
+  // resolve('Resolved!');
+
+  reject('Rejected!');
 });
 
-// create a function which operates just like the 'of' creator function
-function ourOwnOf(...args: string[]): Observable<string> {
-  return new Observable<string>((subscriber) => {
-    for (let index = 0; index < args.length; index++) {
-      subscriber.next(args[index]);
-    }
-    subscriber.complete();
-  });
-}
+const observableFromPromise$ = from(somePromise);
+
+observableFromPromise$.subscribe({
+  next: value => console.log(value),
+  error: err => console.log('Error', err),
+  complete: () => console.log('Completed')
+});
