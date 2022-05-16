e8cdc016af3e53e938f1088ed98c3790eebc143a
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-05-16 13:06:13 -0400

    S3.25 teardown

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { Observable } from 'rxjs';
 
-//S3.24
+//S3.25
 const observable$ = new Observable<string>(subscriber => {  
   console.log('Observable executed');
   subscriber.next('Alice');
   subscriber.next('Ben');
-  setTimeout(() => subscriber.next('Charlie'), 2000);
+  setTimeout(() => {
+    subscriber.next('Charlie');
+    console.log('setting complete event');
+    subscriber.complete();
+}, 2000);
+
+return () => {
+  console.log('Teardown');
+};
 });
 console.log('Before subscribe');
-observable$.subscribe((value) => { console.log('Subscription executed', value); });
+observable$.subscribe({
+  next: (value) => console.log('Subscription executed', value),
+  complete: () => console.log('Subscription completed')
+});
 console.log('After subscribe');
