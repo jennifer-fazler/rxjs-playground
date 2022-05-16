ae433c4a200b2be79e992d1b82391948ac541a24
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-05-16 12:34:29 -0400

    S2.11 warm-up observable, multiple subscriptions

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,14 @@ import { Observable } from 'rxjs';
 const observable$ = new Observable<string>(subscriber => {
   console.log('Observable executed');
   subscriber.next('Alice');
-  // subscriber.next('Ben');
-  // subscriber.next('Charlie');
   setTimeout(() => subscriber.next('Ben'), 2000);
   setTimeout(() => subscriber.next('Charlie'), 4000);
 });
 
-// const observer = {
-//   next: (value:string) => console.log(value)
-// };
+console.log('Subscription 1 starts');
+observable$.subscribe((value:string) => console.log('Subscription 1',value));
 
-// observable$.subscribe(observer);
-
-// this combined version is the one normally used instead of separate pieces as above
-const subscription = observable$.subscribe((value:string) => console.log(value));
-
-// This closes the subscription before we get the 3rd value. (in actual app, you'd want to also stop action that is emitting the values)
 setTimeout(() => {
-  console.log('Unsubscribe');
-  subscription.unsubscribe();
-}, 3000);
+  console.log('Subscription 2 starts');
+  observable$.subscribe((value:string) => console.log('Subscription 2',value));
+  }, 1000);
