28590ca2c3501f3018409dd662d382f174346cfd
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-05-16 13:29:46 -0400

    S3.29 cancellation

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
 import { Observable } from "rxjs";
 
-//S3.28
-const observable$ = new Observable<string>((subscriber) => {
-  console.log("Observable executed");
-  subscriber.next("Alice");
-  subscriber.next("Ben");
-  setTimeout(() => subscriber.error(new Error('Failure')), 2000);
-  setTimeout(() => {
-    subscriber.next("Charlie");
-    subscriber.complete();
-  }, 4000);
+//S3.29
+const interval$ = new Observable<number>((subscriber) => {
+  let counter = 1;
+  const intervalId = setInterval(() => {
+    console.log('emit value: ', counter);
+    subscriber.next(counter++);
+  }, 2000);
 
   return () => {
+    clearInterval(intervalId);
     console.log("Teardown");
   };
 });
-console.log("Before subscribe");
-observable$.subscribe({
-  next: (value) => console.log("Subscription executed", value),
-  error: (err) => console.log(err.message),
-  complete: () => console.log("Subscription completed")
-});
-console.log("After subscribe");
+
+const subscription = interval$.subscribe((value) => console.log('received value: ', value));
+setTimeout(() => {
+  console.log('Unsubscribe');
+  subscription.unsubscribe();
+}, 7000);
