3880c118174e7dff04d8d6d75b21c7b0170a6167
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-05-16 12:50:46 -0400

    S3.22 empty observable

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { Observable } from 'rxjs';
 
+//S3.22
 const observable$ = new Observable<string>(subscriber => {
   console.log('Observable executed');
-  subscriber.next('Alice');
-  setTimeout(() => subscriber.next('Ben'), 2000);
-  setTimeout(() => subscriber.next('Charlie'), 4000);
 });
-
-console.log('Subscription 1 starts');
-observable$.subscribe((value:string) => console.log('Subscription 1',value));
-
-setTimeout(() => {
-  console.log('Subscription 2 starts');
-  observable$.subscribe((value:string) => console.log('Subscription 2',value));
-  }, 1000);
+console.log('Before subscrible');
+observable$.subscribe((value) => { console.log('Subscription executed', value); });
+console.log('After subscrible');
