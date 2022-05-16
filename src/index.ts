6420ab8ebb850bca5b8ad39eaddc94748cf16ea1
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-05-16 11:44:31 -0400

    S2.10 warm up observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { Observable } from 'rxjs';
 
-const someObservable$ = new Observable<string>(subscriber => {
+const observable$ = new Observable<string>(subscriber => {
+  console.log('Observable executed');
   subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-  subscriber.complete();
+  // subscriber.next('Ben');
+  // subscriber.next('Charlie');
+  setTimeout(() => subscriber.next('Ben'), 2000);
+  setTimeout(() => subscriber.next('Charlie'), 4000);
 });
 
-someObservable$.subscribe(value => console.log(value));
+// const observer = {
+//   next: (value:string) => console.log(value)
+// };
+
+// observable$.subscribe(observer);
+
+// this combined version is the one normally used instead of separate pieces as above
+const subscription = observable$.subscribe((value:string) => console.log(value));
+
+// This closes the subscription before we get the 3rd value. (in actual app, you'd want to also stop action that is emitting the values)
+setTimeout(() => {
+  console.log('Unsubscribe');
+  subscription.unsubscribe();
+}, 3000);
