487401c62a2d0b700c2e172f47afc740360c1583
jennifer-fazler <jennifer.fazler@thermofisher.com>
2022-05-16 12:54:37 -0400

    S3.23 next notification

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Observable } from 'rxjs';
 
-//S3.22
-const observable$ = new Observable<string>(subscriber => {
+//S3.23
+const observable$ = new Observable<string>(subscriber => {  
   console.log('Observable executed');
+  subscriber.next('Alice');
 });
 console.log('Before subscrible');
 observable$.subscribe((value) => { console.log('Subscription executed', value); });
